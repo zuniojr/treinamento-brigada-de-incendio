15f81a61b511591a152312e7065dc6e4555fde5b
Anfitriao Airbnb <anfitrioesairbnb@gmail.com>
2026-08-06 18:09:11 -0300

    feat: adicionar novas paginas institucionais e corrigir blog

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,8 +1,29 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async () => {
-  const siteUrl = 'https://treinamentobrigadadeincendio.com.br';
+export const GET: APIRoute = async ({ site }) => {
+  const siteUrl = site ? site.toString().replace(/\/$/, '') : 'https://treinamentobrigadadeincendio.com.br';
   const lastMod = new Date().toISOString().split('T')[0];
+
+  const blogPosts = [
+    'atestado-de-brigada-de-incendio',
+    'broche-brigada-de-incendio',
+    'formacao-de-brigadistas',
+    'validade-do-treinamento-de-brigada-de-incendio',
+    'logo-brigada-de-incendio',
+    'organograma-brigada-de-incendio',
+    'botom-brigada-de-incendio',
+    'treinamento-de-brigada-de-incendio-guia-completo',
+    'brigada-de-incendio-barra-velha-sc',
+    'botoes-brigada-de-incendio',
+    'o-que-e-brigada-de-incendio',
+    'validade-do-treinamento-de-brigada-de-incendio-itajai',
+    'brigada-de-incendio-em-condominios',
+    'curso-de-formacao-de-brigada-de-incendio-em-barra-velha-sc',
+    'curso-de-combate-a-incendio-para-empresas',
+    'treinamento-de-combate-a-incendio-empresarial',
+    'curso-de-brigada-para-empresas-em-barra-velha-sc',
+    'bracadeira-brigada-de-incendio'
+  ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -13,131 +34,81 @@ export const GET: APIRoute = async () => {
     <priority>1.0</priority>
   </url>
   <url>
+    <loc>${siteUrl}/primeiros-socorros</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/treinamento-in-company</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/modulos-personalizados</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/consultoria-tecnica</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/diferenciais</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/contato</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/termos-de-uso</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/politica-de-privacidade</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/consideracoes-importantes</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${siteUrl}/trabalhe-conosco</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
     <loc>${siteUrl}/blog</loc>
     <lastmod>${lastMod}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
   </url>
-  <url>
-    <loc>${siteUrl}/blog/quando-a-empresa-e-obrigada-a-ter-brigada-de-incendio</loc>
+${blogPosts
+  .map(
+    (slug) => `  <url>
+    <loc>${siteUrl}/blog/${slug}</loc>
     <lastmod>${lastMod}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/treinamento-da-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/atestado-de-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/broche-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/formacao-de-brigadistas</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/validade-do-treinamento-de-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/logo-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/organograma-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/botom-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/treinamento-de-brigada-de-incendio-guia-completo</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/brigada-de-incendio-barra-velha-sc</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/botoes-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/o-que-e-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/validade-do-treinamento-de-brigada-de-incendio-itajai</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-<url>
-    <loc>${siteUrl}/blog/brigada-de-incendio-em-condominios</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/curso-de-formacao-de-brigada-de-incendio-em-barra-velha-sc</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/curso-de-combate-a-incendio-para-empresas</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/treinamento-de-combate-a-incendio-empresarial</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/curso-de-brigada-para-empresas-em-barra-velha-sc</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog/bracadeira-brigada-de-incendio</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
+  </url>`
+  )
+  .join('\n')}
 </urlset>`;
 
   return new Response(sitemap, {
